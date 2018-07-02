11b2a3b29ca474fc1f940b6949a6b028b043ba45
Ed Brook <edbrook@gmail.com>
2018-07-02 12:48:41 +0100

    remove unnecessary brackets

--- shuffle.py
+++ shuffle.py
@@ -6,8 +6,8 @@ def shuffle(data):
     n = len(data)
     i = 0
     while i < n:
-        j = randrange(i,n)
-        (data[i],data[j]) = (data[j],data[i])
+        j = randrange(i, n)
+        data[i], data[j] = data[j], data[i]
         i += 1
 
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     def int_array_to_string(data):
         return ','.join(["{:3d}".format(i) for i in my_array])
 
-    my_array = list(range(-5,6,1))
+    my_array = list(range(-5, 6, 1))
     print("Input array:")
     print(int_array_to_string(my_array))
     print("\nShuffled:")
